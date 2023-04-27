df177fbd0e920ee2de0bd3a85c59a3358bd17498
ruthosoro <robinahruth@gmail.com>
2023-04-27 14:58:00 -0700

    printline

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+void print_line(0);
+void print_line(2);
+void print_line(10);
+void print_line(-4);
+return (0);
+}
+
