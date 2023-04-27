5079a1d4873591d65f46446e1b4cce7d761500c0
ruthosoro <robinahruth@gmail.com>
2023-04-27 15:07:26 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+void print_square(2);
+void print_square(10);
+void print_square(0);
+return (0);
+}
+
