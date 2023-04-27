a1c9f24cd87392b93329c3bec1d5ae69077e9bee
ruthosoro <robinahruth@gmail.com>
2023-04-27 15:26:35 -0700

    triangles

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+void print_triangle(2);
+void print_triangle(10);
+void print_triangle(1);
+void print_triangle(0);
+return (0);
+}
+
