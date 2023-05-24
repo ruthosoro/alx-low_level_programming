68ed52606c66af99dbd02697ad595c56648d1888
ruthosoro <robinahruth@gmail.com>
2023-05-24 10:23:50 -0700

    second commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,24 +1,50 @@
-#include <stdio.h>
+#include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
 /**
-* main - initial entry
-* @argc: takes in arguments
-* @argv: number of args
-* Return: 0
+* main - Prints the result of simple operations.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: Always 0.
 */
-int main(int argc, char *argv[])
-{
-	int result;
-
-	if (argc != 4)
+	int main(int __attribute__((__unused__)) argc, char *argv[])
 	{
-		printf("Error\n");
-		exit(98);
+		int num1, num2;
+		char *op;
+
+
+		if (argc != 4)
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+
+		num1 = atoi(argv[1]);
+		op = argv[2];
+		num2 = atoi(argv[3]);
+
+
+		if (get_op_func(op) == NULL || op[1] != '\0')
+		{
+			printf("Error\n");
+			exit(99);
+		}
+
+
+		if ((*op == '/' && num2 == 0) ||
+		    (*op == '%' && num2 == 0))
+		{
+			printf("Error\n");
+			exit(100);
+		}
+
+
+		printf("%d\n", get_op_func(op)(num1, num2));
+
+
+		return (0);
 	}
 
-	result = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
-	printf("%d\n", result);
-
-	return (0);
-}
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,63 +1,67 @@
-#include <stdio.h>
+
 #include "3-calc.h"
-#include <stdlib.h>
-/**
-* op_add - adds a and b
-* @a: takes in an int
-* @b: takes in an int
-* Return: a + b
-*/
-int op_add(int a, int b)
-{
-	return (a + b);
-}
-/**
-* op_sub - subtracts a from b
-* @a: takes in an int
-* @b: takes in an int
-* Return: b - a
-*/
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-/**
-* op_mul - multiple two integers
-* @a: takes in an int
-* @b: takes in an int
-* Return: a * b
-*/
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-/**
-* op_div - function that divides a with b
-* @a: takes in an int
-* @b: takes in an int
-* Return: a / b
-*/
-int op_div(int a, int b)
-{
-	if (b == 0)
+
+
+	int op_add(int a, int b);
+	int op_sub(int a, int b);
+	int op_mul(int a, int b);
+	int op_div(int a, int b);
+	int op_mod(int a, int b);
+
+
+	/**
+	 * op_add - Returns the sum of two numbers.
+	 * @a: The first number.
+	 * @b: The second number.
+	 *
+	 * Return: The sum of a and b.
+	 */
+	int op_add(int a, int b)
 	{
-		printf("Error\n");
-		exit(100);
+		return (a + b);
 	}
-	return (a / b);
-}
-/**
-* op_mod - returns remainder of the division of a by b
-* @a: takes in an int
-* @b: takes in an int
-* Return: a % b
-*/
-int op_mod(int a, int b)
-{
-	if (b == 0)
+	/**
+	 * op_sub - Returns the difference of two numbers.
+	 * @a: The first number.
+	 * @b: The second number.
+	 *
+	 * Return: The difference of a and b.
+	 */
+	int op_sub(int a, int b)
 	{
-		printf("Error\n");
-		exit(100);
+		return (a - b);
 	}
-	return (a % b);
-}
+	/**
+	 * op_mul - Returns the product of two numbers.
+	 * @a: The first number.
+	 * @b: The second number.
+	 *
+	 * Return: The product of a and b.
+	 */
+	int op_mul(int a, int b)
+	{
+		return (a * b);
+	}
+	/**
+	 * op_div - Returns the division of two numbers.
+	 * @a: The first number.
+	 * @b: The second number.
+	 *
+	 * Return: The quotient of a and b.
+	 */
+	int op_div(int a, int b)
+	{
+		return (a / b);
+	}
+	/**
+	 * op_mod - Returns the remainder of the division of two numbers.
+	 * @a: The first number.
+	 * @b: The second number.
+	 *
+	 * Return: The remainder of the division of a by b.
+	 */
+	int op_mod(int a, int b)
+	{
+		return (a % b);
+	}
+
