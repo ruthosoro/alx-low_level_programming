8b3202d80353962394a05d4d1f34c0ab10437b43
ruthosoro <robinahruth@gmail.com>
2023-06-02 10:53:36 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t i = 0;
-while (h != NULL)
-{
-if (h->str)
-printf("[0] (nil)\n");
-else
-printf("[%d] %s\n", h->len, h->str);
-h = h->next;
-i++;
-}
-return i;
+	size_t i = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+
+	return (i);
 }
